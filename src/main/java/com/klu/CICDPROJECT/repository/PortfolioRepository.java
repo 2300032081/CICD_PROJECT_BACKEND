a1b4e7774a1d67f1b7c65e028fbaa8146b38150d
Kavyamrutha Veerla <163729345+2300032081@users.noreply.github.com>
2025-09-26 11:52:07 +0530

    Update PortfolioRepository.java

--- src/main/java/com/klu/CICDPROJECT/repository/PortfolioRepository.java
+++ src/main/java/com/klu/CICDPROJECT/repository/PortfolioRepository.java
@@ -5,14 +5,10 @@ import com.klu.CICDPROJECT.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
+import java.util.List;
 
 @Repository
+ 
 public interface PortfolioRepository extends JpaRepository<Portfolio, Long> {
-
-    // existing method
     Portfolio findByUser(User user);
-
-    // new method - find portfolio by username directly
-    Optional<Portfolio> findByUserUsername(String username);
 }
